2b5f41e9dbb962171650754d0bff68459aada8e3
Sam Espe <sam.espe.sci@gmail.com>
2022-07-08 09:41:12 -0500

    Created outline of how to complete task

---
+++ PyPoll/main.py
@@ -0,0 +1,21 @@
+#Import necessary modules
+import os
+import csv
+
+#Create path for csv file
+
+#Read csv file
+
+#Find number of votes cast
+
+#Find candidates who received votes
+
+#Calculate percentage of votes per candidate
+
+#Find total number of votes per candidate
+
+#Find winner of election
+
+#Print Election Results to terminal
+
+#Create csv file of Election Results
